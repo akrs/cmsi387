0469bfa4e36c6122363918b22c77e3cd3dc7a891
Alex Schneider <aschne19@lion.lmu.edu>
2015-01-26 19:22:16 -0800

    Chapter 3 working

--- cafebabe/loader.s
+++ cafebabe/loader.s
@@ -9,6 +9,7 @@ MEMINFO  equ 1 << 1           ; provide memory map
 FLAGS    equ ALI | MEMINFO    ; the multiboot flags field
 MAGIC    equ 0x1BADB002       ; define the magic number constant
 CHECKSUM equ -(MAGIC + FLAGS) ; calculate the checksum (magic number + flags + checksum should equal 0)
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
 
 section .text:                ; start of the text (code) section
 align 4                       ; the code must be 4 byte aligned
@@ -17,8 +18,23 @@ align 4                       ; the code must be 4 byte aligned
     dd CHECKSUM               ; and the checksum
 
 loader:                       ; the loader label (defined as entry point in linker script)
-    mov eax, 0xCAFEBABE       ; place the number 0xCAFEBABE in the register eax
-    mov ebx, 0xDEADBEEF
-    mov ecx, 0xB16B00B5       ; microsoft compatibility
+;    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+    ; The assembly code
+    extern sum_of_three   ; the function sum_of_three is defined elsewhere
+
+    push dword 3            ; arg3
+    push dword 2            ; arg2
+    push dword 1            ; arg1
+    call sum_of_three       ; call the function, the result will be in eax
+                                                ; stack (end of memory area)
+
+
+
 .loop:
     jmp .loop                 ; loop forever
+
+;section .bss
+;align 4                                     ; align at 4 bytes
+;kernel_stack:                               ; label points to beginning of memory
+;    resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+
