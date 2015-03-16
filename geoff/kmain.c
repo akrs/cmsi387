b7214a006532809cf4b0c9240f79b651e4706e59
Alex Schneider <aschne19@lion.lmu.edu>
2015-03-15 21:57:42 -0700

    Attempt to get modules working

--- geoff/kmain.c
+++ geoff/kmain.c
@@ -4,6 +4,8 @@
 #include <idt.h>
 #include <pic.h>
 #include <io.h>
+#include <multiboot.h>
+#include <stdint.h>
 
 int kernel_main() {
     fb_clear();
@@ -13,8 +15,24 @@ int kernel_main() {
 
     idt_load();
     asm("int 3");
+    
 
     setup_pic();
 
-    return 42;
+    multiboot_info_t *mbinfo;
+    asm("mov %0, ebx" : "=g" (mbinfo));
+    uintptr_t address_of_module = mbinfo->mods_addr;
+    fb_write_hex(address_of_module);
+    asm("xchg bx, bx");
+    typedef void (*call_module_t)(void);
+    call_module_t start_program = (call_module_t) &address_of_module;
+    start_program+=0;
+    fb_write_str("Flags: ");
+    fb_write_num(mbinfo->flags);
+    fb_write_str("\nmods_count: ");
+    fb_write_num(mbinfo->mods_count);
+    asm("xchg bx, bx");
+    start_program();
+
+    return 0;
 }
--- geoff/loader.s
+++ geoff/loader.s
@@ -4,18 +4,15 @@
 ; instead
 global loader                 ; the entry symbol for ELF
 
-ALI    equ 1 << 0             ; align loaded modules on page boundaries
-MEMINFO  equ 1 << 1           ; provide memory map
-FLAGS    equ ALI | MEMINFO    ; the multiboot flags field
-MAGIC    equ 0x1BADB002       ; define the magic number constant
-CHECKSUM equ -(MAGIC + FLAGS) ; calculate the checksum (magic number + flags + checksum should equal 0)
+MAGIC           equ 0x1BADB002       ; define the magic number constant
+ALIGN_MODULES   equ 0x00000001
+CHECKSUM equ -(MAGIC + ALIGN_MODULES) ; calculate the checksum (magic number + flags + checksum should equal 0)
 KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
 
 section .text                ; start of the text (code) section
-boot:
 align 4                       ; the code must be 4 byte aligned
     dd MAGIC                  ; write the magic number to the machine code
-    dd FLAGS
+    dd ALIGN_MODULES
     dd CHECKSUM               ; and the checksum
 
 loader:                       ; the loader label (defined as entry point in linker script)
