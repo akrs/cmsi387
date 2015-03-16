c62f500da9c4b1af29c4aed27ae1140d28ef31a1
Alex Schneider <aschne19@lion.lmu.edu>
2015-03-15 22:35:28 -0700

    Work some more

--- geoff/kmain.c
+++ geoff/kmain.c
@@ -7,7 +7,7 @@
 #include <multiboot.h>
 #include <stdint.h>
 
-int kernel_main() {
+int kernel_main(uintptr_t ebx) {
     fb_clear();
     fb_write_str("Hello world, I'm Geoff.\n");
 
@@ -19,18 +19,18 @@ int kernel_main() {
 
     setup_pic();
 
-    multiboot_info_t *mbinfo;
-    asm("mov %0, ebx" : "=g" (mbinfo));
+    multiboot_info_t *mbinfo = (multiboot_info_t *)ebx;
     uintptr_t address_of_module = mbinfo->mods_addr;
     fb_write_hex(address_of_module);
     asm("xchg bx, bx");
     typedef void (*call_module_t)(void);
     call_module_t start_program = (call_module_t) address_of_module;
-    start_program+=0;
-    fb_write_str("Flags: ");
+    fb_write_str("\nFlags: ");
     fb_write_num(mbinfo->flags);
     fb_write_str("\nmods_count: ");
     fb_write_num(mbinfo->mods_count);
+    fb_write_str("Start program: ");
+    fb_write_hex((int32_t)start_program);
     asm("xchg bx, bx");
     start_program();
 
